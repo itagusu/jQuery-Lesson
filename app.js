1f3891892ce486315edfd7436c3a4ae420e6a26a
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-14 11:53:42 +0900

    jQuery

--- app.js
+++ app.js
@@ -33,12 +33,39 @@
 //   $('.box1').hide();
 // });
 
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//       'background-color':'#0000FF',
+//       'height':'200px',
+//       'width':'200px',
+//     }).slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color':'#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color':'#FF0000'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color':'#0000FF',
-      'height':'200px',
-      'width':'200px',
-    }).slideUp();
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout('click',function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
