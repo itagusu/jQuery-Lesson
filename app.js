52d35b1bc745f5f9457a3be2edfa6fbbc4f1410e
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-14 11:29:14 +0900

    jQuery

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
+// $(function(){
+//  $('.box1').slideDown(function(){
+//   $('.box1').css({
+//     'background-color':'#0000ff',
+//     'height':'100px'
+//   }).slideUp();
+//  });
+// });
+
+// $(function(){
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px',
+//     'width': '100px'
+//   });
+// });
+
+// $(function(){
+//   $('.box1').slideDown();
+  
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color':'#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').hide();
+// });
+
 $(function(){
- $('.box1').slideDown(function(){
-  $('.box1').css({
-    'background-color':'#0000ff',
-    'height':'100px'
-  }).slideUp();
- });
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color':'#0000FF',
+      'height':'200px',
+      'width':'200px',
+    }).slideUp();
+  });
 });
